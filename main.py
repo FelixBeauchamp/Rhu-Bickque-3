090b23f045f5a9e29a0e1b500eab4152bb978f9f
FelixBeuachamp <fx.beauchamp@gmail.com>
2024-01-17 13:15:15 -0500

    Libraries

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # This is a sample Python script.
+from Adafruit_PCA9685 import PCA9685
+from dynamixel_sdk import protocol1_packet_handler
 
 # Press Maj+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -12,5 +14,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+
+    Servo1 = PCA9685
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
