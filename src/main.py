6b66ea2d2072da4ef07ca2881c484e9e17ce57ef
Ethan Beaudoin <beaudoin.ethan@gmail.com>
2024-02-07 12:59:01 -0500

    add class algo

--- src/main.py
+++ src/main.py
@@ -1,31 +1,6 @@
 import serial.tools.list_ports
-
-ports = serial.tools.list_ports.comports()
-serialInst = serial.Serial()
-
-portsList = []
-
-portVar = None
-
-for onePort in ports:
-    portsList.append(str(onePort))
-    print(str(onePort))
-
-val = input("Select Port: COM")
-
-for x in range(0, len(portsList)):
-    if portsList[x].startswith("COM" + str(val)):
-        portVar = "COM" + str(val)
-        print(portVar)
-
-serialInst.baudrate = 9600
-serialInst.port = portVar
-serialInst.open()
+from Algo_Cube import Algo
+algo_cool = Algo()
 
 while True:
-    command = input("Enter command: ")
-    if command == 'exit':
-        exit()
-    serialInst.write(command.encode('utf-8'))
-    response = serialInst.readline().decode().strip()
-    print(f"Arduino response: {response}")
+    algo_cool.hello_world()
