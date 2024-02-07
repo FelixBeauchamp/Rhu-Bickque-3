77d6fd8b73467e04e2b704af594d07f687fe758a
Ethan Beaudoin <beaudoin.ethan@gmail.com>
2024-02-07 15:19:11 -0500

    add real class algo

--- src/Algo_Cube.py
+++
@@ -1,4 +0,0 @@
-# This file create the movements sequence allowing to solve the cube
-class Algo:
-    def hello_world(self) -> None:
-        print("Hello world")
--- src/main.py
+++ src/main.py
@@ -1,6 +1,4 @@
 import serial.tools.list_ports
-from Algo_Cube import Algo
-algo_cool = Algo()
 
 while True:
-    algo_cool.hello_world()
+    print("balls")
