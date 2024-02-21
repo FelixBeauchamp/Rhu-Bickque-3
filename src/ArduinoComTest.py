dddbcd38d4e51b6b66094763786ea69ffe11f76c
BiboFou <bilb1602@usherbrooke.ca>
2024-02-21 09:40:00 -0500

    update com Arduino-Pycharm

---
+++ src/ArduinoComTest.py
@@ -0,0 +1,31 @@
+import serial
+import time
+def sendtomega(megawhat):
+
+    # Open the serial port (adjust the port and baud rate as needed)
+    ser = serial.Serial('COM3', 9600)
+
+    ser.close()
+    ser.open()
+
+    time.sleep(1)
+    # Send a string to Arduino
+    message = megawhat
+    ser.write(message.encode())
+
+    # Wait for a short time to ensure Arduino has processed the input
+    time.sleep(1)
+
+    # Read the response from Arduino
+    response = ser.readline().decode()
+
+    # Print the response
+    print("Response from Arduino:", response)
+
+    # Close the serial port
+    ser.close()
+
+    return response
+
+sendtomega('pushPair1')
+
