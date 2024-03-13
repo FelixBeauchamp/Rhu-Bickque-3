855100c702c5c01f84b1c84ea1cb5a9022c012d1
Xavier Gervais <xavgervais@gmail.com>
2024-03-12 21:10:16 -0400

    modification determination de couleurs et clean up du file traitement image

--- src/traitement image.py
+++ src/traitement image.py
@@ -1,57 +1,61 @@
 # The file to analyze the colors on the cube
 import numpy as np
 import cv2
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
-YellowL_limit = np.array([30, 100, 70])  # setting the yellow lower limit
-YellowU_limit = np.array([40, 255, 255])  # setting the yellow upper limit
+YellowL_limit = np.array([25, 80, 0])  # setting the yellow lower limit
+YellowU_limit = np.array([39, 255, 194])  # setting the yellow upper limit
 
-BlueL_limit = np.array([100, 100, 70])  # setting the yellow lower limit
-BlueU_limit = np.array([140, 255, 255])
+BlueL_limit = np.array([110, 80, 0])  # setting the yellow lower limit
+BlueU_limit = np.array([130, 255, 194])
 
-RedL_limit = np.array([165, 150, 160])  # setting the yellow lower limit
-RedU_limit = np.array([180, 255, 255])
+RedL_limit = np.array([141, 80, 0])  # setting the yellow lower limit
+RedU_limit = np.array([180, 255, 194])
 
-OrangeL_limit = np.array([10, 200, 70])  # setting the yellow lower limit
-OrangeU_limit = np.array([25, 255, 255])
+OrangeL_limit = np.array([10, 80, 0])  # setting the yellow lower limit
+OrangeU_limit = np.array([25, 255, 194])
 
-GreenL_limit = np.array([30, 30, 30])  # setting the yellow lower limit
-GreenU_limit = np.array([70, 255, 255])
+GreenL_limit = np.array([40, 80, 0])  # setting the yellow lower limit
+GreenU_limit = np.array([100, 255, 194])
 
-WhiteL_limit = np.array([30, 100, 70])  # setting the yellow lower limit
-WhiteU_limit = np.array([50, 255, 255])
+WhiteL_limit = np.array([0, 0, 120])  # setting the yellow lower limit
+WhiteU_limit = np.array([180, 79, 255])
 
 
 # frames = np.array([[1,1,1],[1,1,1],[1,1,1]])
 def colorofsquare(leframe):
     Y_mask = cv2.inRange(leframe, YellowL_limit, YellowU_limit)
     yellow_pixel_count = cv2.countNonZero(Y_mask)
-    if yellow_pixel_count > 500:
-        return 1
-    else:
-        B_mask = cv2.inRange(leframe, BlueL_limit, BlueU_limit)
-        blue_pixel_count = cv2.countNonZero(B_mask)
-        if blue_pixel_count > 1000:
-            return 2
-        R_mask = cv2.inRange(leframe, RedL_limit, RedU_limit)
-        red_pixel_count = cv2.countNonZero(B_mask)
-        if red_pixel_count > 1000:
-            return 3
-        O_mask = cv2.inRange(leframe, OrangeL_limit, OrangeU_limit)
-        orange_pixel_count = cv2.countNonZero(O_mask)
-        if orange_pixel_count > 1000:
-            return 4
-        G_mask = cv2.inRange(leframe, GreenL_limit, GreenU_limit)
-        green_pixel_count = cv2.countNonZero(G_mask)
-        if green_pixel_count > 1000:
-            return 5
-        W_mask = cv2.inRange(leframe, WhiteL_limit, WhiteU_limit)
-        white_pixel_count = cv2.countNonZero(W_mask)
-        if white_pixel_count > 1000:
-            return 6
+    cv2.imshow('Yellow pixel count',Y_mask)
+    B_mask = cv2.inRange(leframe, BlueL_limit, BlueU_limit)
+    blue_pixel_count = cv2.countNonZero(B_mask)
 
+    R_mask = cv2.inRange(leframe, RedL_limit, RedU_limit)
+    red_pixel_count = cv2.countNonZero(R_mask)
 
-while 1:
+    O_mask = cv2.inRange(leframe, OrangeL_limit, OrangeU_limit)
+    orange_pixel_count = cv2.countNonZero(O_mask)
+
+    G_mask = cv2.inRange(leframe, GreenL_limit, GreenU_limit)
+    green_pixel_count = cv2.countNonZero(G_mask)
+
+    W_mask = cv2.inRange(leframe, WhiteL_limit, WhiteU_limit)
+    white_pixel_count = cv2.countNonZero(W_mask)
+
+    couleur = max(blue_pixel_count, red_pixel_count, orange_pixel_count, green_pixel_count, yellow_pixel_count, white_pixel_count)
+    if couleur == blue_pixel_count:
+        return "B"
+    elif couleur == red_pixel_count:
+        return "R"
+    elif couleur == orange_pixel_count:
+        return "O"
+    elif couleur == green_pixel_count:
+        return "G"
+    elif couleur == yellow_pixel_count:
+        return "Y"
+    elif couleur == white_pixel_count:
+        return "W"
+def faceofdacube():
     ret, frame = cap.read()
     width = np.size(frame, 1)
     height = np.size(frame, 0)
@@ -85,7 +89,8 @@ while 1:
 
     into_hsv = cv2.cvtColor(filtered, cv2.COLOR_BGR2HSV)
     into_hsv_filtered = cv2.cvtColor(filtered, cv2.COLOR_BGR2HSV)
-
+    frame_cube = into_hsv[haut_1_x:bas_3_x, gauche_x_1:droite_x_3].copy()
+    cv2.imshow('1_1', frame_cube)
     frame_1_1 = into_hsv[haut_1_x:bas_1_x, gauche_x_1:droite_x_1].copy()
     frame_2_1 = into_hsv[haut_2_x:bas_2_x, gauche_x_1:droite_x_1].copy()
     frame_3_1 = into_hsv[haut_3_x:bas_3_x, gauche_x_1:droite_x_1].copy()
@@ -112,10 +117,10 @@ while 1:
     green = cv2.bitwise_and(frame, frame, mask=G_mask)
     white = cv2.bitwise_and(frame, frame, mask=W_mask)
     '''
-    # this will give the color to mask.
-    cv2.imshow('frame1_1', frame_1_1)
-    '''
+
+
     cv2.imshow('Original', frame)  # to display the original frame
+    '''
     cv2.imshow('Filtered', filtered)  # to display the original frame
     cv2.imshow('hsv', into_hsv)  # to display the original frame
     cv2.imshow('hsv_filtered', into_hsv_filtered)  # to display the original frame
@@ -153,13 +158,14 @@ while 1:
     square2_3 = colorofsquare(frame_2_3)
     square3_3 = colorofsquare(frame_3_3)
 
-    print(square1_1, square1_2, square1_3)
-    print("\n")
-    print(square2_1, square2_2, square2_3)
-    print("\n")
-    print(square3_1, square3_2, square3_3)
+    daresults = [square1_1, square1_2, square1_3], [square2_1, square2_2, square2_3],[square3_1, square3_2, square3_3]
 
+    return daresults
+
+
+while 1:
     #
+    print(faceofdacube())
     if cv2.waitKey(3000) == 27:
         break
     # this function will be triggered when the ESC key is pressed
