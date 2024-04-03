35db32bf460ff5c61f900a383d84b5a1a919b91c
Ethan Beaudoin <beaudoin.ethan@gmail.com>
2024-04-03 14:59:53 -0400

    integration ui part 9

--- src/control.py
+++ src/control.py
@@ -1,6 +1,5 @@
 import ArduinoCom
 import OpenRBCom
-import UI
 import traitement_image
 import keyboard
 import time
@@ -18,15 +17,28 @@ port_Arduino = 'COM7'
 port_OpenRB = 'COM6'
 
 
+def initialisation():
+    print("Initialisation: OPENING/HOMING")
+    OpenRBCom.openport(port_OpenRB)
+    ArduinoCom.openportarduino(port_Arduino)
+    ArduinoCom.sendmessage('OXOY')
+    time.sleep(4)
+    OpenRBCom.sendmessage('HOMING')
+
+
+def clamp():
+    input("Press Enter to clamp the cube")
+    ArduinoCom.sendmessage('CXCY')
+
+
 def mapping_sequence():
+    input("Press Enter to start the mapping sequence")
     mapp = ['O', 'B', 'B', 'G', 'G', 'Y', 'O', 'G', 'R', 'W', 'B', 'W', 'Y', 'W', 'G', 'B', 'W', 'W',
             'B', 'B', 'Y', 'O', 'B', 'B', 'G', 'Y', 'G', 'G', 'R', 'B', 'W', 'Y', 'G', 'R', 'O', 'G',
             'R', 'R', 'Y', 'W', 'O', 'W', 'Y', 'R', 'Y', 'O', 'Y', 'O', 'O', 'R', 'R', 'W', 'O', 'R']
     # Iterate through the camera motor sequence and analizing the
     map_array = [[[0] * 3 for _ in range(3)] for _ in range(6)]
     # for move in solver.sequence_camera:
-    #     if UI.stop_pressed:
-    #         break
     #     if move[0] == 'M':
     #         OpenRBCom.sendmessage(move)
     #     elif move[0] == 'S':
@@ -35,71 +47,43 @@ def mapping_sequence():
     #         mapp.extend(temp)
     #     else:
     #         ArduinoCom.sendmessage(move)
-    if not UI.stop_pressed:
-        map_array = solver.reformat(mapp)
-        cb = cube.Cube(map_array)
-        print(cb)
-        UI.mapping_array = map_array
+    map_array = solver.reformat(mapp)
+    cb = cube.Cube(map_array)
+    print(cb)
     return map_array
 
 
-def solving(map_array):
+def solving_moves(map_array):
     c = cube.Cube(map_array)
     print("Solving:\n", c)
     solution = solver.Solver(c)
     solution.solveCube(optimize=True)
-    moves = solution.getMoves(decorated=True)
-    print(moves)
+    mov = solution.getMoves(decorated=True)
+    print(mov)
 
-    moves_list = solver.Solver.reformat(moves)
+    moves_list = solver.Solver.reformat(mov)
     print(f"{len(moves_list)} moves: {' '.join(moves_list)}")
     solution.translate(moves_list)
     print(f"{len(solution.sequence_motors)} moves: {' '.join(solution.sequence_motors)}")
 
-    for moves in solution.sequence_motors:
-        if UI.stop_pressed:
-            break
-        input('bibos dick')
-        if moves[0] == 'M' or moves[0] == 'H':
-            OpenRBCom.sendmessage(moves)
-        else:
-            ArduinoCom.sendmessage(moves)
+    return solution.sequence_motors
 
 
-def change_state(state):
-    UI.SolvingState = state
+def do_move(move):
+    input('bibos dick')
+    if move[0] == 'M' or move[0] == 'H':
+        OpenRBCom.sendmessage(move)
+    else:
+        ArduinoCom.sendmessage(move)
+
+
+def close_ports():
+    ArduinoCom.closeportarduino()
+    OpenRBCom.closeport()
 
 
 if __name__ == '__main__':
-    # 0- Initialisation
-    change_state(0)
-    if not UI.stop_pressed:
-        print("Initialisation: OPENING/HOMING")
-        OpenRBCom.openport(port_OpenRB)
-        ArduinoCom.openportarduino(port_Arduino)
-        ArduinoCom.sendmessage('OXOY')
-        time.sleep(4)
-        OpenRBCom.sendmessage('HOMING')
-
-    # 1- Wait for user input before clamping the cube
-    if not UI.stop_pressed:
-        change_state(1)
-        input("Press Enter to clamp the cube")
-        ArduinoCom.sendmessage('CXCY')
-
-    # 2- Starting the mapping sequence
-    if not UI.stop_pressed:
-        change_state(2)
-        input("Press Enter to start the mapping sequence")
-        cube_map = mapping_sequence()
-
-    # 3- Solving the cube
-    if not UI.stop_pressed:
-        change_state(3)
-        input("Press Enter to start solving")
-        solving(cube_map)
-        ArduinoCom.closeportarduino()
-        OpenRBCom.closeport()
+    print("MEGAWHAAAT")
 
     # print("Initialisation: OPENING/HOMING")
     # OpenRBCom.openport(port_OpenRB)
