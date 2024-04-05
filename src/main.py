a2dbab515d601a79a5ad60bee0431cc799cdb049
Ethan Beaudoin <beaudoin.ethan@gmail.com>
2024-04-05 13:58:58 -0400

    testing2

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import sys
 if __name__ == '__main__':
     # Initialize face colors to all white
     initial_face_colors = {
-        "Back": ['red'] * 9,
+        "B  ack": ['red'] * 9,
         "Left": ['green'] * 9,
         "Top": ['yellow'] * 9,
         "Right": ['blue'] * 9,
