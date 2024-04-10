8dc7fd869bceebb2531fbbad882bd11d10a38fa1
Xavier Gervais <xavgervais@gmail.com>
2024-04-10 10:25:14 -0400

    Test carre plus petit traitement image

--- src/traitement_image.py
+++ src/traitement_image.py
@@ -95,7 +95,7 @@ def faceofdacube():
     #coin_inf_droit = [width, height]
 
     cote_cube = 85
-
+    frame_loose = 15
     # Barre horizontales
     haut_1_x = 100
     bas_1_x = haut_1_x+cote_cube
@@ -125,25 +125,25 @@ def faceofdacube():
     frame_cube_avg = into_hsv_filtered_avg[haut_1_x:bas_3_x, gauche_x_1:droite_x_3].copy()
     cv2.imshow('Face du cube avg', frame_cube_avg)
 
-    frame_1_1 = into_hsv_filtered_avg[haut_1_x:bas_1_x, gauche_x_1:droite_x_1].copy()
+    frame_1_1 = into_hsv_filtered_avg[haut_1_x+frame_loose:bas_1_x-frame_loose, gauche_x_1+frame_loose:droite_x_1-frame_loose].copy()
     cv2.imshow('Face 1_1', frame_1_1)
-    frame_2_1 = into_hsv_filtered_avg[haut_2_x:bas_2_x, gauche_x_1+length_pince:droite_x_1].copy()
+    frame_2_1 = into_hsv_filtered_avg[haut_2_x+frame_loose:bas_2_x-frame_loose, gauche_x_1+length_pince:droite_x_1].copy()
     cv2.imshow('Face 2_1', frame_2_1)
-    frame_3_1 = into_hsv_filtered_avg[haut_3_x:bas_3_x, gauche_x_1:droite_x_1].copy()
+    frame_3_1 = into_hsv_filtered_avg[haut_3_x+frame_loose:bas_3_x-frame_loose, gauche_x_1+frame_loose:droite_x_1-frame_loose].copy()
     cv2.imshow('Face 3_1', frame_3_1)
 
-    frame_1_2 = into_hsv_filtered_avg[haut_1_x+length_pince:bas_1_x, gauche_x_2:droite_x_2].copy()
+    frame_1_2 = into_hsv_filtered_avg[haut_1_x+length_pince:bas_1_x, gauche_x_2+frame_loose:droite_x_2-frame_loose].copy()
     cv2.imshow('Face 1_2', frame_1_2)
-    frame_2_2 = into_hsv_filtered_avg[haut_2_x:bas_2_x, gauche_x_2:droite_x_2].copy()
+    frame_2_2 = into_hsv_filtered_avg[haut_2_x+frame_loose:bas_2_x-frame_loose, gauche_x_2+frame_loose:droite_x_2-frame_loose].copy()
     cv2.imshow('Face 2_2', frame_2_2)
-    frame_3_2 = into_hsv_filtered_avg[haut_3_x:bas_3_x-length_pince, gauche_x_2:droite_x_2].copy()
+    frame_3_2 = into_hsv_filtered_avg[haut_3_x+frame_loose:bas_3_x-length_pince, gauche_x_2+frame_loose:droite_x_2-frame_loose].copy()
     cv2.imshow('Face 3_2', frame_3_2)
 
-    frame_1_3 = into_hsv_filtered_avg[haut_1_x:bas_1_x, gauche_x_3:droite_x_3].copy()
+    frame_1_3 = into_hsv_filtered_avg[haut_1_x+frame_loose:bas_1_x-frame_loose, gauche_x_3+frame_loose:droite_x_3-frame_loose].copy()
     cv2.imshow('Face 1_3', frame_1_3)
-    frame_2_3 = into_hsv_filtered_avg[haut_2_x:bas_2_x, gauche_x_3:droite_x_3-length_pince].copy()
+    frame_2_3 = into_hsv_filtered_avg[haut_2_x+frame_loose:bas_2_x-frame_loose, gauche_x_3:droite_x_3-length_pince].copy()
     cv2.imshow('Face 2_3', frame_2_3)
-    frame_3_3 = into_hsv_filtered_avg[haut_3_x:bas_3_x, gauche_x_3:droite_x_3].copy()
+    frame_3_3 = into_hsv_filtered_avg[haut_3_x+frame_loose:bas_3_x-frame_loose, gauche_x_3+frame_loose:droite_x_3-frame_loose].copy()
     cv2.imshow('Face 3_3', frame_3_3)
 
     square1_1 = colorofsquare(frame_1_1)
